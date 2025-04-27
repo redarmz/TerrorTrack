aef4136e237f93457ba648757728afb469edfa55
amiirmmr <mammeriamir4@gmail.com>
2025-04-27 22:25:16 +0200

    modele avec un réseau de neurones

--- backend/instant_backend/api/urls.py
+++ backend/instant_backend/api/urls.py
@@ -3,5 +3,5 @@ from .views import predict_risk
 
 urlpatterns = [
     path("predict/", predict_risk, name="predict"),
-    path("test", lambda request: JsonResponse({"ok": True})), 
+    path("test/", lambda request: JsonResponse({"ok": True})), 
 ]
